0e8e271d38fe26ec73ccd3997b69b6a9432e573a
emrekokcu1 <emrekokcu1453@gmail.com>
2024-04-28 15:46:19 +0300

    added

--- src/test/java/com/kokcu/utils/MyFirstProgram.java
+++ src/test/java/com/kokcu/utils/MyFirstProgram.java
@@ -3,6 +3,8 @@ package com.kokcu.utils;
 public class MyFirstProgram {
     public static void main(String[] args) {
         String welcome = "Welcome Abdullah Uzundere";
+        //prints welcome message
         System.out.println(welcome);
+
     }
 }
